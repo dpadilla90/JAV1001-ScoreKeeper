3d00080966346e537eb9d9e3b17d8c87332e8663
Dennis Padilla <d_padilla.y@hotmail.com>
2023-07-26 12:42:36 -0400

    Implemented view binding

--- app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -2,54 +2,50 @@ package com.example.scorekeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.RadioGroup
-import android.widget.TextView
+import com.example.scorekeeper.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     // Declare private properties for UI elements and scores
-    private lateinit var increaseButton1: Button
-    private lateinit var increaseButton2: Button
-    private lateinit var decreaseButton1: Button
-    private lateinit var decreaseButton2: Button
-    private lateinit var incrementOptionsRadioGroup: RadioGroup
+    private lateinit var binding: ActivityMainBinding
     private var score1 = 0
     private var score2 = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Set up data binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         // Change the title text programmatically
         val newTitle = "Scorekeeper App"
-        setTitle(newTitle)
+        supportActionBar?.setTitle(newTitle)
 
-        // Initialize UI elements by finding their respective views in the layout
-        increaseButton1 = findViewById(R.id.increase_button_t1)
-        increaseButton2 = findViewById(R.id.increase_button_t2)
-        decreaseButton1 = findViewById(R.id.decrease_button_t1)
-        decreaseButton2 = findViewById(R.id.decrease_button_t2)
-        incrementOptionsRadioGroup = findViewById(R.id.incrementOptionsRadioGroup)
 
         // Set click listeners for buttons to handle score updates
-        increaseButton1.setOnClickListener {
+        binding.increaseButtonT1.setOnClickListener {
             // Increase the score of team 1 based on the selected increment value
             score1 += getSelectedIncrementValue()
             updateScoreTextView()
         }
-        decreaseButton1.setOnClickListener {
+
+        binding.increaseButtonT2.setOnClickListener {
+            // Increase the score of team 2 based on the selected increment value
+            score2 += getSelectedIncrementValue()
+            updateScoreTextView()
+        }
+
+        binding.decreaseButtonT1.setOnClickListener {
             // Decrease the score of team 1 by 1 if the score is greater than 0
             if (score1 > 0) {
                 score1--
             }
             updateScoreTextView()
         }
-        increaseButton2.setOnClickListener {
-            // Increase the score of team 2 based on the selected increment value
-            score2 += getSelectedIncrementValue()
-            updateScoreTextView()
-        }
-        decreaseButton2.setOnClickListener {
+
+        binding.decreaseButtonT2.setOnClickListener {
             // Decrease the score of team 2 by 1 if the score is greater than 0
             if (score2 > 0) {
                 score2--
@@ -60,15 +56,13 @@ class MainActivity : AppCompatActivity() {
 
     // Update the score text views with the current scores
     private fun updateScoreTextView() {
-        val scoreTextView1: TextView = findViewById(R.id.scoreTextView1)
-        scoreTextView1.text = score1.toString()
-        val scoreTextView2: TextView = findViewById(R.id.scoreTextView2)
-        scoreTextView2.text = score2.toString()
+        binding.scoreTextView1.text = score1.toString()
+        binding.scoreTextView2.text = score2.toString()
     }
 
     // Get the selected increment value from the radio button group
     private fun getSelectedIncrementValue(): Int {
-        return when (incrementOptionsRadioGroup.checkedRadioButtonId) {
+        return when (binding.incrementOptionsRadioGroup.checkedRadioButtonId) {
             R.id.option1RadioButton -> 1
             R.id.option2RadioButton -> 2
             R.id.option3RadioButton -> 3
@@ -76,3 +70,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
